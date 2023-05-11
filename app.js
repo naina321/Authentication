e33f9f3a1bba27fba1582bb1886df36e923bd786
naina321 <naina1910078@akgec.ac.in>
2023-05-11 14:41:04 +0530

    register and login done

--- app.js
+++ app.js
@@ -1,19 +1,55 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 // app.set("views", __dirname + "/public");
+mongoose.connect("mongodb://localhost:27017/secrets");
+const trySchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+const item = mongoose.model("second", trySchema);
 
 app.get("/", (req, res) => {
   res.render("partials/home");
 });
+
+app.post("/register", (req, res) => {
+  const newUser = new item({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save();
+  res.render("partials/secrets");
+});
+
 app.get("/register", (req, res) => {
   res.render("partials/register");
 });
+
+app.post("/login", async (req, res) => {
+  try {
+    const username = req.body.userName;
+    const password = req.body.password;
+    await item.findOne({ email: username }, (err, foundUser) => {
+      if (err) console.log(err);
+      else {
+        if (foundUser) {
+          if (foundUser.password === password) res.render("partials/secrets");
+          else res.send("possword is incorrect");
+        } else res.send("user does not exist!!");
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 app.get("/login", (req, res) => {
   res.render("partials/login");
 });
